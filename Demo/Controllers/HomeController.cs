09a7d4e57262c27fc65db630ba10c8789ac9908e
ITCity <ITCity@DESKTOP-MBNODK0>
2022-06-01 12:06:33 +0430

    delete logger

--- Demo/Controllers/HomeController.cs
+++ Demo/Controllers/HomeController.cs
@@ -11,11 +11,11 @@ namespace Demo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+        
         }
 
         public IActionResult Index()
